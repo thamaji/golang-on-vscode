4f47b493e88fd9abcae5bfb42ed65bf5288015e9
Takahisa HAMAJI <hamaji_takahisa@kurusugawa.jp>
2021-09-24 18:29:51 +0900

    docker.sockのgidがすでに存在する環境に対応

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -eu
 
-# docker.sock から gid を取得して、docker グループの gid を変更
+# docker.sock から gid を取得して、vscode ユーザーがそのグループに所属するようにする
 docker_group_id=$(ls -n /var/run/docker.sock | cut -d ' ' -f 4)
-sudo groupmod --gid ${docker_group_id} docker
+getent group ${docker_group_id} || sudo groupadd -g ${docker_group_id} docker
+sudo usermod -aG ${docker_group_id} vscode
 
 # 無限待ち（vscode デフォルトの entrypoint と同じ方法で）
 while sleep 1000; do :; done
